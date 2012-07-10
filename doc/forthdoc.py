15118c9f6b0173b055f42cb5963a9c561aa128f0
Markus Redeker <cep@ibp.de>
2012-07-10 19:25:31 +0100

    "forthdoc.py" now uses a domain "forth". Added separate index for Forth.

--- doc/forthdoc.py
+++ doc/forthdoc.py
@@ -1,9 +1,15 @@
 # Documentation module for Forth vocabularies
 
 import re
+import string
+import itertools
 
 from docutils import nodes
 from sphinx import addnodes
+from sphinx.directives import ObjectDescription
+from sphinx.domains import Index, Domain, ObjType
+from sphinx.roles import XRefRole
+from sphinx.util.nodes import make_refnode
 from sphinx.util.compat import Directive
 
 
@@ -46,8 +52,55 @@ def parse_worddef(env, sig, signode):
     signode += nodes.emphasis(say, say)
     signode += extras(3)
     return name
-    
-class Source(Directive):
+
+def forth_sortname(name):
+    match = re.match('([^a-zA-Z0-9]+)([a-zA-Z0-9].*)$', name)
+    if match:
+        pair = match.group(2), match.group(1)
+    else:
+        pair = name, u''
+
+    c = pair[0][0]
+    if c in string.letters or c in string.digits:
+        return (1,) + pair
+    else:
+        return (0,) + pair
+
+def forth_heading(c):
+    if c in string.letters or c in string.digits:
+        return c.upper()
+    else:
+        return 'Symbols'
+
+class ForthWord(object):
+
+    def __init__(self, name, docname):
+        self.name = name
+        self.fullname = 'word-' + name
+        self.docname = docname
+
+        self.sortname = forth_sortname(name)
+        self.heading = forth_heading(self.sortname[1][0])
+        # self.heading = self.sortname[0][0].upper()
+
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, self.name)
+
+    def __cmp__(self, other):
+        return cmp(self.sortname, other.sortname)
+
+class WordDirective(ObjectDescription):
+
+    def handle_signature(self, sig, signode):
+        return parse_worddef(self.env, sig, signode)
+
+    def add_target_and_index(self, name_cls, sig, signode):
+        word = ForthWord(name_cls, self.env.docname)
+
+        signode['ids'].append(word.fullname)
+        self.env.domaindata['forth']['words'][name_cls] = word
+
+class SourceDirective(Directive):
 
     required_arguments = 1
     optional_arguments = 0
@@ -59,9 +112,61 @@ class Source(Directive):
         return [nodes.paragraph(),
                 nodes.emphasis(text='Source: '),
                 nodes.inline(text=self.arguments[0] + '.')]
+
+class ForthXRefRole(XRefRole):
+    pass
+
+class ForthIndex(Index):
+
+    name = 'words'
+    localname = 'Index of Forth Words'
+    shortname = 'words'
+
+    def generate(self, docnames=None):
+        wordlist = self.domain.env.domaindata['forth']['words']
+        words = sorted(wordlist.itervalues())
+
+        content = []
+        for head, g in itertools.groupby(words, lambda w: w.heading):
+            entries = []
+            for w in g:
+                entries += [(w.name, 0, w.docname, w.fullname, '', '', '')]
+            content += [(head, entries)]
+        return (content, True)
+
+class ForthDomain(Domain):
+
+    name = 'forth'
+    label = 'Forth'
+    object_types = {        # type (usually directive) name -> ObjType instance
+        'word': ObjType('word', 'word'),
+        }
+    directives = {                      #: directive name -> directive class
+        'word': WordDirective,
+        'source': SourceDirective
+        }
+    roles = {                           # role name -> role callable
+        'word' : ForthXRefRole()
+        }
+    indices = [ForthIndex]              # a list of Index subclasses
+    initial_data = {                    # data value for a fresh environment
+        'words': {}                     # name -> ForthWord(name)
+        }
+
+    def resolve_xref(self, env, fromdocname, builder,
+                     typ, target, node, contnode):
+        try:
+            word = env.domaindata['forth']['words'][target]
+            return make_refnode(builder, fromdocname,
+                                word.docname, word.fullname, contnode, target)
+        except KeyError:
+            return
+
  
 def setup(app):
     # *app* is a Sphinx object in sphinx/application.py
 
     app.add_object_type('word', 'word', 'single: %s', parse_node=parse_worddef)
-    app.add_directive('source', Source)
+    app.add_directive('source', SourceDirective)
+
+    app.add_domain(ForthDomain)
